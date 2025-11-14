8e596e7c03a0cafee3ed807fa51993983756decc
Zain Kizilbash <zainkizilbash8832@gmail.com>
2025-11-14 17:36:43 +0500

    added mui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import { List, ListItem, ListItemText, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 
 function App() {
@@ -9,14 +10,16 @@ function App() {
     .then(data => setActivities(data))
   },[])           //run only once on mount
   return (
-    <div>
-      <h3>Reactivities</h3>
-      <ul>
+    <>
+      <Typography variant='h3'>Reactivities</Typography>
+      <List>
         {activities.map((activity) => (
-          <li key={activity.id}>{activity.title}</li>
+          <ListItem key={activity.id}>
+            <ListItemText>{activity.title}</ListItemText>
+          </ListItem>
         ))}
-      </ul>
-    </div>
+      </List>
+    </>
   )
 }
 
