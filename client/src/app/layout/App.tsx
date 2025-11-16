6674ce14bc04f38675fd3eced43f9c8dc8154ebc
Zain Kizilbash <zainkizilbash8832@gmail.com>
2025-11-17 00:52:00 +0500

    styling of navbar

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,4 +1,4 @@
-import { CssBaseline, List, ListItem, ListItemText } from "@mui/material";
+import { Container, CssBaseline, List, ListItem, ListItemText } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Navbar from "./Navbar";
@@ -8,20 +8,22 @@ function App() {
 
   useEffect(() => {
     axios.get<Activity[]>('https://localhost:5001/api/activities')
-    .then(response => setActivities(response.data))
-  },[])           //run only once on mount
+      .then(response => setActivities(response.data))
+  }, [])           //run only once on mount
   return (
     <>
       {/* this stretches the navbar all the way to the top and left and right aswell*/}
-      <CssBaseline/>          
-      <Navbar/>
-      <List>
-        {activities.map((activity) => (
-          <ListItem key={activity.id}>
-            <ListItemText>{activity.title}</ListItemText>
-          </ListItem>
-        ))}
-      </List>
+      <CssBaseline />
+      <Navbar />
+      <Container maxWidth='xl' sx={{ mt: 3 }}>
+        <List>
+          {activities.map((activity) => (
+            <ListItem key={activity.id}>
+              <ListItemText>{activity.title}</ListItemText>
+            </ListItem>
+          ))}
+        </List>
+      </Container>
     </>
   )
 }
