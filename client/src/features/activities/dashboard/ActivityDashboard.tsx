a8f22039c37a9f565965bcc622fbc6f9ad92a882
Zain Kizilbash <zainkizilbash8832@gmail.com>
2025-11-23 02:27:07 +0500

    created the activity form for creating/deleting an activity

--- client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,6 +1,7 @@
 import { Grid2 } from "@mui/material";
 import ActivityList from "./ActivityList";
 import ActivityDetails from "../details/ActivityDetails";
+import ActivityForm from "../form/ActivityForm";
 
 type Props = {
     activities: Activity[];
@@ -24,6 +25,7 @@ export default function ActivityDashboard({ activities, selectActivity,
                     activity={selectedActivity}
                     cancelSelectActivity={cancelSelectActivity}
                 />}
+                <ActivityForm />
             </Grid2>
         </Grid2>
     )
