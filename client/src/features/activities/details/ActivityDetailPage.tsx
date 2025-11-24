e0268c623cdbba400fa403ba4f552d910cae532d
Zain Kizilbash <zainkizilbash8832@gmail.com>
2025-11-24 13:35:01 +0500

    adding content for the Activity Details

--- client/src/features/activities/details/ActivityDetailPage.tsx
+++ client/src/features/activities/details/ActivityDetailPage.tsx
@@ -16,8 +16,8 @@ export default function ActivityDetails() {
     return (
         <Grid2 container spacing={3}>
             <Grid2 size={8}>
-                <ActivityDetailsHeader />
-                <ActivityDetailsInfo />
+                <ActivityDetailsHeader activity={activity} />
+                <ActivityDetailsInfo activity={activity} />
                 <ActivityDetailsChat />
             </Grid2>
             <Grid2 size={4}>
