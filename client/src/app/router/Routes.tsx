3150f33b392db425d2310d042f60bb5f9dfd06bd
Zain Kizilbash <zainkizilbash8832@gmail.com>
2025-11-24 13:03:34 +0500

    styling the activity details component

--- client/src/app/router/Routes.tsx
+++ client/src/app/router/Routes.tsx
@@ -3,7 +3,7 @@ import App from "../layout/App";
 import HomePage from "../../features/home/HomePage";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import ActiviyForm from "../../features/activities/form/ActivityForm";
-import ActivityDetails from "../../features/activities/details/ActivityDetails";
+import ActivityDetailPage from "../../features/activities/details/ActivityDetailPage";
 
 export const router = createBrowserRouter([
     {
@@ -13,7 +13,7 @@ export const router = createBrowserRouter([
             { path: '', element: <HomePage /> },
             { path: 'activities', element: <ActivityDashboard /> },
             { path: 'createActivity', element: <ActiviyForm key='create' /> },
-            { path: 'activities/:id', element: <ActivityDetails /> },
+            { path: 'activities/:id', element: <ActivityDetailPage /> },
             { path: 'manage/:id', element: <ActiviyForm /> },
         ]
     }
