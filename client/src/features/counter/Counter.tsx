595151244771ec42bd8d4617f08aa288c104b247
Zain Kizilbash <zainkizilbash8832@gmail.com>
2025-11-30 00:11:03 +0500

    MobX Actions to update store values

--- client/src/features/counter/Counter.tsx
+++ client/src/features/counter/Counter.tsx
@@ -1,18 +1,27 @@
-import { Typography } from "@mui/material";
+import { Button, ButtonGroup, Typography } from "@mui/material";
 import { useStore } from "../../lib/hooks/useStore";
 import { Observer } from "mobx-react-lite";
 
 export default function Counter() {
     const { counterStore } = useStore();
     return (
-        <Observer>
-            {() => (
-                <>
-                    <Typography variant="h4" gutterBottom>{counterStore.title}</Typography>
-                    <Typography variant="h6">The count is: {counterStore.title}</Typography>
+        <>
+            <Observer>
+                {() => (
+                    <>
+                        <Typography variant="h4" gutterBottom>{counterStore.title}</Typography>
+                        <Typography variant="h6">The count is: {counterStore.count}</Typography>
 
-                </>
-            )}
-        </Observer>
+                    </>
+                )}
+            </Observer>
+            <ButtonGroup sx={{ mt: 3 }}>
+                <Button onClick={() => counterStore.decrement()} variant="contained" color="error">Decrement</Button>
+                <Button onClick={() => counterStore.increment()} variant="contained" color="success">Increment</Button>
+                <Button onClick={() => counterStore.increment(5)} variant="contained" color="primary">Increment By 5</Button>
+
+            </ButtonGroup>
+        </>
+
     )
 }
