08a926291fcc87ca621ba33684287f2bacfb19b0
Zain Kizilbash <zainkizilbash8832@gmail.com>
2025-12-06 23:58:41 +0500

    using an axios interceptor to handle errors

--- client/src/lib/api/agent.ts
+++ client/src/lib/api/agent.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { store } from "../stores/store";
+import { toast } from "react-toastify";
 
 const sleep = (delay: number) => {
     return new Promise(resolve => {
@@ -16,19 +17,35 @@ agent.interceptors.request.use(config => {
     return config;
 })
 
-agent.interceptors.response.use(async response => {
-    try{
-        //just delaying the api response forcefully
+agent.interceptors.response.use(
+    async response => {
         await sleep(1000);
+        store.uiStore.isIdle()
         return response;
-    }
-    catch (error){
-        console.log(error);
-        return Promise.reject(error);
-    } finally {
-         store.uiStore.isIdle();
+    },
+    async error => {
+        await sleep(1000);
+        store.uiStore.isIdle();
 
+        const {status} = error.response;
+        switch(status) {
+            case 400:
+                toast.error("bad request")
+                break;
+            case 401:
+                toast.error('Unauthorized');
+                break;
+            case 404:
+                toast.error('Not found');
+                break;
+            case 500:
+                toast.error('Server error');
+                break;
+            default:
+                break;
+        }
+        return  Promise.reject(error);
     }
-})
+)
 
 export default agent;
