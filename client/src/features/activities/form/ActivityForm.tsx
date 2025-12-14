ee6a44998e7d9875e6f034e252ead50cbb0b404e
Zain Kizilbash <zainkizilbash8832@gmail.com>
2025-12-14 13:46:58 +0500

    form submission

--- client/src/features/activities/form/ActivityForm.tsx
+++ client/src/features/activities/form/ActivityForm.tsx
@@ -1,6 +1,6 @@
 import { Box, Button, Paper, Typography } from "@mui/material";
 import { useActivities } from "../../../lib/hooks/useActivities";
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import { useForm } from 'react-hook-form';
 import { useEffect } from "react";
 import { activitySchema, type ActivitySchema } from "../../../lib/schemas/activitySchema";
@@ -16,15 +16,39 @@ export default function ActiviyForm() {
         mode: 'onTouched',
         resolver: zodResolver(activitySchema)
     });            //react-hook-form built in
+    const navigate = useNavigate();
     const { id } = useParams();
     const { updateActivity, createActivity, activity, isLoadingActivity } = useActivities(id);
 
     useEffect(() => {
-        if (activity) reset(activity);
+        if (activity) reset({
+            ...activity,
+            location: {
+                city: activity.city,
+                venue: activity.venue,
+                latitude: activity.latitude,
+                longitude: activity.longitude
+            }
+        });
     }, [activity, reset]);
 
-    const onSubmit = (data: ActivitySchema) => {
-        console.log(data)
+    const onSubmit = async (data: ActivitySchema) => {
+        const {location, ...rest} = data;
+        const flattenedData = {...rest, ...location};
+        try {
+            if (activity) {
+                updateActivity.mutate({...activity, ...flattenedData}, {
+                    onSuccess: () => navigate(`/activites/${activity.id}`)
+                })
+            } else {
+                createActivity.mutate(flattenedData, {
+                    onSuccess: (id) => navigate(`/activities/${id}`)
+                })
+            }
+
+        } catch(error){
+            console.log(error);
+        }
     }
 
     if (isLoadingActivity) return <Typography>Loading...</Typography>
@@ -37,9 +61,11 @@ export default function ActiviyForm() {
                 <TextInput label='Title' control={control} name='title' />
                 <TextInput label='Description' control={control} name='description'
                     multiline rows={3} />
-                <SelectInput items={categoryOptions}
-                    label='Category' control={control} name='category' />
-                <DateTimeInput label='Date' control={control} name='date' />
+                <Box display='flex' gap={3}>
+                    <SelectInput items={categoryOptions}
+                        label='Category' control={control} name='category' />
+                    <DateTimeInput label='Date' control={control} name='date' />
+                </Box>
                 <LocationInput control={control} label='Enter the location' name="location" />
 
                 <Box display='flex' justifyContent='end' gap={3}>
