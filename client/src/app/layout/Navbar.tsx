9a815eaf7f49b4b2f5c80b3b612589ee620d0f28
Zain Kizilbash <zainkizilbash8832@gmail.com>
2025-12-20 01:16:08 +0500

    creating a user menu

--- client/src/app/layout/Navbar.tsx
+++ client/src/app/layout/Navbar.tsx
@@ -5,6 +5,7 @@ import MenuItemLink from "../shared/components/MenuItemLink";
 import { useStore } from "../../lib/hooks/useStore";
 import { Observer } from "mobx-react-lite";
 import { useAccount } from "../../lib/hooks/useAccount";
+import UserMenu from "./UserMenu";
 
 export default function Navbar() {
   const { uiStore } = useStore();
@@ -28,9 +29,6 @@ export default function Navbar() {
               <MenuItemLink to='/activities'>
                 Activites
               </MenuItemLink>
-              <MenuItemLink to='/createActivity'>
-                Create Activity
-              </MenuItemLink>
               <MenuItemLink to='/counter'>
                 Counter
               </MenuItemLink>
@@ -40,7 +38,7 @@ export default function Navbar() {
             </Box>
             <Box display='flex' alignItems='center'>
                 {currentUser ? (
-                  <Typography>Welcome {currentUser.displayName}</Typography>
+                  <UserMenu/>
                 ): (
                   <>
                     <MenuItemLink to='/login'>Login</MenuItemLink>
---
+++ client/src/app/layout/UserMenu.tsx
@@ -0,0 +1,62 @@
+import { Avatar, Box, Divider, ListItem, ListItemText } from '@mui/material';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import { useState } from 'react';
+import { useAccount } from '../../lib/hooks/useAccount';
+import { Link } from 'react-router';
+import { Logout, Person } from '@mui/icons-material';
+
+export default function UserMenu() {
+    const {currentUser, logoutUser} = useAccount();
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <>
+      <Button
+        onClick={handleClick}
+        color='inherit'
+        size='large'
+        sx={{fontSize: '1.1rem'}}
+      >
+        <Box display='flex' alignItems='center' gap={2}>
+            <Avatar/>
+            {currentUser?.displayName}
+        </Box>
+      </Button>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem component={Link} to = '/profile' onClick={handleClose}>
+            <ListItem>
+                <Person/>
+            </ListItem>
+            <ListItemText>My profile</ListItemText>
+        </MenuItem>
+        <Divider/>
+        <MenuItem onClick={() => {
+            logoutUser.mutate();
+            handleClose();
+        }}>
+            <ListItem>
+                <Logout/>
+            </ListItem>
+            <ListItemText>Logout</ListItemText>
+        </MenuItem>
+      </Menu>
+    </>
+  );
+}
