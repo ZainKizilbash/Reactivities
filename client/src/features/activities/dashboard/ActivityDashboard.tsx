c5444e074fe2fb9cc23bd94a6d9497a6cd3b2068
Zain Kizilbash <zainkizilbash8832@gmail.com>
2026-03-01 04:42:05 +0500

    setting up client pagination part 2

--- client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,17 +1,23 @@
-import { Grid2, Typography } from "@mui/material";
+import { Button, Grid2 } from "@mui/material";
 import ActivityList from "./ActivityList";
 import { useActivities } from "../../../lib/hooks/useActivities";
 import ActivityFilters from "./ActivityFilters";
 
 export default function ActivityDashboard() {
-    const { activities } = useActivities();
-
-    if (!activities) return <Typography>Loading...</Typography>
+    const { isFetchingNextPage, fetchNextPage, hasNextPage } = useActivities();
 
     return (
         <Grid2 container spacing={3}>
             <Grid2 size={8}>
                 <ActivityList />
+                <Button
+                    onClick={() => fetchNextPage}
+                    sx={{my: 2, float: 'right'}}
+                    variant="contained"
+                    disabled={!hasNextPage || isFetchingNextPage}
+                >
+                    Load more
+                </Button>
             </Grid2>
             <Grid2 size={4}>
                 <ActivityFilters />
